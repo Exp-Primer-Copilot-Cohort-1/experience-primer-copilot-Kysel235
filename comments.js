45996bda0fa4589ddaf356ef695a114f0ebcb366
Kysel235 <72470207+Kysel235@users.noreply.github.com>
2024-04-02 20:22:15 +0000

    Copilot third commit v3

--- comments.js
+++ comments.js
@@ -1,46 +1,78 @@
 // create web server
-// create a new instance of express
-const express = require('express');
-const app = express();
-// create a new instance of http
-const http = require('http');
-// create a new instance of socket.io
-const socketIo = require('socket.io');
-
-// create a new http server
-const server = http.createServer(app);
-// create a new socket.io server
-const io = socketIo(server);
-
-// the port we are listening to
-const port = 3000;
-
-// middleware
-app.use(express.static(__dirname + '/public'));
-
-// the route for the server
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var ROOT = __dirname + "/public";
+var comments = require('./comments.json');
+var log = require('./log')(module);
+var server = http.createServer(function(req, res) {
+    if (!checkAccess(req)) {
+        res.statusCode = 403;
+        res.end("Tell me the secret to access!");
+        return;
+    }
+    sendFileSafe(url.parse(req.url).pathname, res);
 });
 
-// the server listens for a connection
-io.on('connection', (socket) => {
-    console.log('a user connected');
+server.listen(3000);
 
-    // the server listens for a message
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg);
-        // the server sends the message to all connected clients
-        io.emit('message', msg);
+function checkAccess(req) {
+    return url.parse(req.url, true).query.secret == 'o_O';
+}
+
+function sendFileSafe(filePath, res) {
+    try {
+        filePath = decodeURIComponent(filePath);
+    } catch (e) {
+        res.statusCode = 400;
+        res.end("Bad Request");
+        return;
+    }
+
+    if (~filePath.indexOf('\0')) {
+        res.statusCode = 400;
+        res.end("Bad Request");
+        return;
+    }
+
+    filePath = path.normalize(path.join(ROOT, filePath));
+
+    if (filePath.indexOf(ROOT) != 0) {
+        res.statusCode = 404;
+        res.end("File not found");
+        return;
+    }
+
+    fs.stat(filePath, function(err, stats) {
+        if (err || !stats.isFile()) {
+            res.statusCode = 404;
+            res.end("File not found");
+            return;
+        }
+
+        sendFile(filePath, res);
     });
+}
 
-    // the server listens for a disconnection
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+function sendFile(filePath, res) {
+    fs.readFile(filePath, function(err, content) {
+        if (err) throw err;
+        var mime = require('mime').lookup(filePath);
+        res.setHeader('Content-Type', mime + "; charset=utf-8");
+        res.end(content);
+    });
+}
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function(socket) {
+    socket.emit('comments', comments);
+    socket.on('comment', function(comment) {
+        comments.push(comment);
+        fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+            if (err) throw err;
+        });
+        io.sockets.emit('comment', comment);
     });
 });
-
-// the server listens for connections on the port
-server.listen(port, () => {
-    console.log('listening on *:' + port);
-});
+log.info('Server running on port 3000');
